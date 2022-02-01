7c0d9d9d203154d6ae3b8c6125cf0332adb464d9
luke-meadows <lukemeadowsdev@gmail.com>
2022-02-01 16:33:59 +0000

    first commit

---
+++ howMuchIsTrue.js
@@ -0,0 +1,12 @@
+// Create a function which returns the number of true values there are in an array.
+
+// Examples
+// countTrue([true, false, false, true, false]) ➞ 2
+
+// countTrue([false, false, false, false]) ➞ 0
+
+// countTrue([]) ➞ 0
+
+function countTrue(arr) {
+  return arr.filter((x) => x).length;
+}
